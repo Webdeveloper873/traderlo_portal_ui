a31b73dac02388aa97e306e64cc6c8c2fa34517c
Angelica Capuz <angelica@stebr.com>
2020-01-20 21:11:03 +0800

    Changed styling for Subscribe Newsletter

--- src/components/Home/SubscribeLetter.jsx
+++ src/components/Home/SubscribeLetter.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Row, Jumbotron, InputGroup, Form, Button, Col, Image} from 'react-bootstrap';
 
+//components
+import PageWrapper from 'common/components/PageWrapper';
+
 //styles
 import classes from './styles.module.scss';
 
@@ -26,26 +29,30 @@ const DetailsList = ({imgSrc, title, subTitle}) => {
 
 const SubscribeLetter = () => {
   return(
-    <Row className={classes.contentRow}>
-      <Jumbotron className={`${classes.subsNewLetter} justify-content-md-center`}>
-        <h1>Subscribe To Newsletter</h1>
-        <span className={classes.subTitle}>Get notified about the next update</span>
-        <Col lg={{span: 6, offset: 3}} className={`mb-3`}>
-          <InputGroup>
-            <Form.Control aria-describedby='basic-addon1' />
-            <InputGroup.Append>
-              <Button variant='primary'>Search</Button>
-            </InputGroup.Append>
-          </InputGroup>
-        </Col>
-        <Row>
-          <DetailsList imgSrc={NewsLetter1} title='35,682' subTitle='Active Listings'/>
-          <DetailsList imgSrc={NewsLetter2} title='99500' subTitle='Users'/>
-          <DetailsList imgSrc={NewsLetter3} title='45,824,890' subTitle='Item Sold'/>
-          <DetailsList imgSrc={NewsLetter4} title='12,068' subTitle='Transactions Done'/>
-        </Row>
+    <div className={classes.contentRow}>
+      <Jumbotron className={`${classes.subsNewLetter}`}>
+        <PageWrapper>
+          <div className={classes.fullWidth}>
+            <h1>Subscribe To Newsletter</h1>
+            <span className={classes.subTitle}>Get notified about the next update</span>
+            <Col lg={{span: 6, offset: 3}} className={`mb-3`}>
+              <InputGroup>
+                <Form.Control aria-describedby='basic-addon1' />
+                <InputGroup.Append>
+                  <Button variant='primary'>Search</Button>
+                </InputGroup.Append>
+              </InputGroup>
+            </Col>
+          </div>
+          <Row className={classes.fullWidth}>
+            <DetailsList imgSrc={NewsLetter1} title='35,682' subTitle='Active Listings'/>
+            <DetailsList imgSrc={NewsLetter2} title='99500' subTitle='Users'/>
+            <DetailsList imgSrc={NewsLetter3} title='45,824,890' subTitle='Item Sold'/>
+            <DetailsList imgSrc={NewsLetter4} title='12,068' subTitle='Transactions Done'/>
+          </Row>
+        </PageWrapper>
       </Jumbotron>
-    </Row>
+    </div>
   );
 }
 
