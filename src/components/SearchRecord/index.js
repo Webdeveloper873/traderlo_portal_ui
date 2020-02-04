61f38bcd5c2e6876814364416ea54fe2c5df9135
Angelica Capuz <angelica@stebr.com>
2020-02-04 21:15:30 +0800

    Updated filters

--- src/components/SearchRecord/index.js
+++ src/components/SearchRecord/index.js
@@ -1,104 +1,106 @@
 import React, { useState } from 'react';
-import { Card, Col, Button, DropdownButton, Dropdown } from 'react-bootstrap';
-import { Slider, Input } from 'antd';
+import { Col } from 'react-bootstrap';
+import { Slider, Divider, Card, Typography, Select, Checkbox } from 'antd';
 
 //components
 import PageWrapper from 'common/components/PageWrapper';
-import Divider from 'common/components/Divider';
+// import Divider from 'common/components/Divider';
 import Banner from './Banner';
 
 //styles
 import classes from './styles.module.scss';
 
+const {Text} = Typography;
+const {Option} = Select;
+
+const SliderFilter = ({ sliderVal, amtLabel, ...props}) => {
+  return(
+    <>
+      {sliderVal ? <span>{`${sliderVal[0]} ${amtLabel || ''} to ${sliderVal[1]} ${amtLabel || ''}`}</span> : null}
+      <Slider range
+        step={1}
+        max={20}
+        defaultValue={[0, 20]}
+        {...props}
+      />
+    </>
+  )
+}
+
 const Filters = () => {
-  const [sliderRange, setSliderRange] = useState([0, 20]);
-  const [domainRange, setDomainRange] = useState([0, 20]);
-  const [timeRange, setTimeRage] = useState([0, 20]);
+  const [domainPrice, setDomainPrice] = useState([0, 20]);
+  const [domainAge, setDomainAge] = useState([0, 20]);
+  const [domainLength, setDomainLength] = useState([0, 20]);
+  const [timeRemaining, setTimeRemaining] = useState([0, 20]);
 
-  const sliderOnChange = (value) => {
-    console.log('onChange: ', value);
-    setSliderRange(value);
+  const onChangeDomPrice = (value) => {
+    setDomainPrice(value);
   }
 
-  const domainSliderOnChange = (value) => {
-    setDomainRange(value);
+  const onChangeDomAge = value => {
+    setDomainAge(value);
   }
-  
-  const timeSliderOnChange = (value) => {
-    setTimeRage(value);
+
+  const onChangeDomLength = value => {
+    setDomainLength(value);
+  }
+
+  const onChangeTimeRem = value => {
+    setTimeRemaining(value);
   }
 
   return(
-    <Card className={classes.filters} body>
-      <div className='d-flex justify-content-between'>
-        <span className={classes.label}>Filters</span>
-        <Button variant="outline-info border-0">clear all</Button>
-      </div>
-      <Divider className={classes.divider} />
-      <span className={classes.filterLabel}>Placement</span>
-      <select className={`browser-default custom-select ${classes.inputField}`} defaultValue='1'>
-        <option value="1">Anywhere</option>
-        <option value="2">At the start</option>
-        <option value="3">At the end</option>
-        <option value="3">Exact</option>
-      </select>
-      <span className={classes.filterLabel}>Extensions</span>
-      <select className={`browser-default custom-select ${classes.inputField}`}>
-        <option value="1">.com</option>
-        <option value="2">.net</option>
-        <option value="3">.org</option>
-        <option value="3">.io</option>
-      </select>
-      <span>{`${sliderRange[0]} - ${sliderRange[1]}`}</span>
-      <Slider
-        range
-        step={1}
-        max={20}
-        defaultValue={[0, 20]}
-        onChange={sliderOnChange}
-      />
+    <>
+      <Card className={classes.filters}>
+        <Text strong>Domain Price</Text>
+        <Divider className={classes.divider} />
+        <SliderFilter sliderVal={domainPrice} onChange={(onChangeDomPrice)} />
 
-      <span className={classes.filterLabel}>Price Range</span>
-      <Input style={{ width: 90, textAlign: 'center' }} placeholder="Minimum" />
-      <Input
-        style={{
-          width: 30,
-          borderLeft: 0,
-          pointerEvents: 'none',
-          backgroundColor: '#fff',
-        }}
-        placeholder="~"
-        disabled
-      />
-      <Input style={{ width: 90, textAlign: 'center', borderLeft: 0 }} placeholder="Maximum" />
+        <Text strong>Domain Age</Text>
+        <Divider className={classes.divider} />
+        <SliderFilter sliderVal={domainAge} onChange={onChangeDomAge} />
 
-      <span className={classes.filterLabel}>Domain Age</span>
-      <span>{`${domainRange[0]} month - ${domainRange[1]} years`}</span>
-      <Slider
-        range
-        step={1}
-        max={20}
-        defaultValue={[0, 20]}
-        onChange={domainSliderOnChange}
-      />
+        <Text strong>Domain Length</Text>
+        <Divider className={classes.divider} />
+        <SliderFilter sliderVal={domainLength} onChange={onChangeDomLength} amtLabel={'years'}/>
+      </Card>
 
-      <span className={classes.filterLabel}>Include Only</span>
-      <select className={`browser-default custom-select ${classes.inputField}`} defaultValue='1'>
-        <option value="1">Nothing Selected</option>
-        <option value="2">Reserve Met</option>
-        <option value="3">But It Now</option>
-      </select>
+      <Card className={`${classes.filters} ${classes.filterCard}`}>
+        <Text strong>Extensions</Text>
+        <Divider className={classes.divider} />
+        <Select className={classes.inputField} placeholder='- Select -'>
+          <Option value=".com">.com</Option>
+          <Option value=".in">.in</Option>
+          <Option value=".co">.co</Option>
+        </Select>
 
-      <span className={classes.filterLabel}>Time Remaining</span>
-      <span>{`${timeRange[0]} Day(s) - ${timeRange[1]} Days`}</span>
-      <Slider
-        range
-        step={1}
-        max={45}
-        defaultValue={[0, 45]}
-        onChange={timeSliderOnChange}
-      />
-    </Card>
+        <Text strong>Include Only</Text>
+        <Divider className={classes.divider} />
+        <Checkbox.Group>
+          <Col>
+            <Checkbox value='reverseMet'>Reverse Met</Checkbox>
+          </Col>
+          <Col>
+            <Checkbox value='buyNow'>Buy It Now</Checkbox>
+          </Col>
+        </Checkbox.Group>
+      </Card>
+
+      <Card className={`${classes.filters} ${classes.filterCard}`}>
+        <Text strong>Time Remaining</Text>
+        <Divider className={classes.divider} />
+        <SliderFilter sliderVal={timeRemaining} onChange={onChangeTimeRem} amtLabel={'years'} />
+      </Card>
+
+      <Card className={`${classes.filters} ${classes.filterCard}`}>
+        <Text strong>Listing Type</Text>
+        <Divider className={classes.divider} />
+        <Select className={classes.inputField} placeholder='- Select -'>
+          <Option value="editorsChoice">Editor's Choice</Option>
+          <Option value="regOption">Regular Options</Option>
+        </Select>
+      </Card>
+    </>
   );
 }
 
