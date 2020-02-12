5b262e8644089ff0398cab0ea7bb0cad514cd34c
Angelica Capuz <angelica@stebr.com>
2020-02-12 21:59:43 +0800

    Finalized Promote tab design

--- src/components/SellingSub/components/Promote.jsx
+++ src/components/SellingSub/components/Promote.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
 import { Button, Card, Col, Row } from 'antd';
 
-//components
-import InputField from './InputField';
-
 //styles
 import classes from '../styles.module.scss';
 
 //resources
 import EditorChoice from 'assets/selling/editorchoice.png';
 import Featured from 'assets/selling/featured.png';
+import FreeBasic from 'assets/selling/freebasic.png';
 
-const PromoteCard = ({imgSrc, label}) => {
+const PromoteCard = ({imgSrc, label, details}) => {
   return(
-    <Card className={classes.promoteCard}
-      bodyStyle={{ background: '#f9f9f9' }}
-      title={<>
-        <h5>Editor Choice</h5>
-        <img src={imgSrc} alt='...loading' />
-      </>}>
-      <div className={classes.cardFooter}>
-        An auction will get you the highest price and the most bids.
-      </div>
-    </Card>
+    <Col xs={24} md={8}>
+      <Card className={classes.promoteCard}
+        bodyStyle={{ background: '#f9f9f9' }}
+        title={<>
+          <h5>{label}</h5>
+          <img src={imgSrc} alt='...loading' />
+        </>}>
+        <div className={classes.cardFooter}>{details}</div>
+      </Card>
+    </Col>
   );
 }
 
@@ -30,12 +28,15 @@ const Promote = () => {
   return(
     <Card>
       <Row gutter={16}>
-        <Col xs={24} md={8}>
-          <PromoteCard imgSrc={EditorChoice} label='Editor Choice' />
-        </Col>
-        <Col xs={24} md={8}>
-          <PromoteCard imgSrc={Featured} label='Featured' />
-        </Col>
+        <PromoteCard imgSrc={FreeBasic} label='Free Basic'
+          details='Promote your listing for free, At no cost.'
+        />
+        <PromoteCard imgSrc={Featured} label='Featured'
+          details='Be first to sell your listing at $50Per Month'
+        />
+        <PromoteCard imgSrc={EditorChoice} label='Editor Choice'
+          details='Be first to sell your listing at $50Per Month'
+        />
       </Row>
       <Row className={classes.btnContainer}>
         <Button type='primary' size='large' className={classes.btnStyle}>Next</Button>
