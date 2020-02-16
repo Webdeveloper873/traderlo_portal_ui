f753a3cb9752d704dd191e7266dc26b9e54c62b3
Angelica Capuz <angelica@stebr.com>
2020-02-16 19:41:51 +0800

    Added domain name

--- src/components/Domains/components/DomainDetails/index.js
+++ src/components/Domains/components/DomainDetails/index.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Row, Col, Card } from 'antd';
+
+//components
+import Banner from 'common/components/Banner';
+import PageWrapper from 'common/components/PageWrapper';
+
+//styles
+import classes from './styles.module.scss';
+
+const LeftPane = () => {
+  return(
+    <Col xs={24} md={16}>
+      <Card className={classes.cardStyle}>
+        <h3>Domain Name</h3>
+      </Card>
+    </Col>
+  );
+}
+
+const RightPane = () => {
+  return(
+    <Col xs={24} md={8}>
+      <Card>
+      </Card>
+    </Col>
+  );
+}
 
 const DomainDetails = () => {
   return(
-    <div>
-      DomainDetails
-    </div>
+    <>
+      <Banner text='Payment' />
+      <PageWrapper>
+        <Row className={classes.rowStyle} gutter={16}>
+          <LeftPane />
+          <RightPane />
+        </Row>
+      </PageWrapper>
+    </>
   );
 }
 
