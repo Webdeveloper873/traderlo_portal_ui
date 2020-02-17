96633ba13712b9a1716139075a000ca0f6b84f4b
Angelica Capuz <angelica@stebr.com>
2020-02-17 20:28:31 +0800

    Added number, address and chat

--- src/components/Domains/components/DomainDetails/components/SellerDetails.jsx
+++ src/components/Domains/components/DomainDetails/components/SellerDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { List, Avatar } from 'antd';
+import { List, Avatar, Card, Icon, Button, Row, Col } from 'antd';
 
 //styles
 import classes from '../styles.module.scss';
@@ -17,13 +17,33 @@ const Description = () => {
 
 const SellerDetails = () => {
   return(
-    <List.Item key={1}>
-      <List.Item.Meta
-        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-        title={<a href="https://ant.design">{'item.name.last'}</a>}
-        description={<Description />}
-      />
-    </List.Item>
+    <>
+      <List.Item key={1}>
+        <List.Item.Meta
+          avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+          title={<a href="https://ant.design">{'item.name.last'}</a>}
+          description={<Description />}
+        />
+      </List.Item>
+      <Card className={`${classes.sellerDetailsCard} ${classes.phoneNumberCard}`}>
+        <Icon type="lock" className={classes.padlock}/>
+        <span className={classes.number}>(+1) 234 677 8899</span>
+        <span className={classes.showNumber}>Click to Reveal Phone No.</span>
+      </Card>
+      <Card className={`${classes.sellerDetailsCard} ${classes.addressCard}`}>
+        <Icon type="pushpin" className={classes.padlock} />
+        <span className={classes.number}>ManChester, Uk</span>
+      </Card>
+      <Row justify='space-between' className={classes.rowStyle} type='flex'>
+        <Col span={12}>
+          <Button type='primary'>View All Listings</Button>
+        </Col>
+        <Col span={12}>
+          <Icon type="message" theme='twoTone' className={classes.messageIcon}/>
+          <span className={classes.chat}>CHAT</span>
+        </Col>
+      </Row>
+    </>
   );
 }
 
