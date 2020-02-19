22414aa4d923f227f8e3d1e795e53a68317e81f2
Angelica Capuz <angelica@stebr.com>
2020-02-19 21:11:10 +0800

    Added route to My Bids

---
+++ src/components/UserBuyActivity/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Row, Col} from 'antd';
+
+//components
+import Banner from 'common/components/Banner';
+import PageWrapper from 'common/components/PageWrapper';
+
+const UserBuyActivity = () => {
+  return(
+    <>
+      <Banner text={'Bids'} />
+      <PageWrapper>
+      <Row>
+        <Col>
+        </Col>
+       </Row>
+      </PageWrapper>
+    </>
+  );
+}
+
+export default UserBuyActivity;
