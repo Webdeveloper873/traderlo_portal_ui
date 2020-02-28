7ae263d6ce19933640f1213640a78d0ec040b7be
Angelica Capuz <angelica@stebr.com>
2020-02-28 10:44:52 +0800

    Restructured payment component

---
+++ src/common/components/Payments/Components/ChooseMethod/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Avatar, Divider, Row, Col, Button, Steps } from 'antd';
+
+//styles
+import classes from '../../styles.module.scss';
+
+//assets
+import bankCheck from 'assets/payments/bankCheck.png';
+import debitCards from 'assets/payments/debitCards.png';
+import paypal from 'assets/payments/paypal.png';
+
+const ChooseMethod = () => {
+  return(
+    <>
+      <span className={classes.subHeader}>Select the Payment Method to make the payment</span>
+      <Row className={classes.imageRow}>
+        <Col span={8}>
+          <Row><Avatar shape="square" size={120} icon="user" src={debitCards} /></Row>
+          <Row><span className={classes.fontDecor}> Credit / Debit Card</span></Row>
+        </Col>
+        <Col span={8}>
+          <Row><Avatar shape="square" size={120} icon="user" src={bankCheck} /></Row>
+          <Row><span className={classes.fontDecor}> Bank Account </span></Row>
+        </Col>
+        <Col span={8}>
+          <Row>{' '}</Row>
+          <Row><Avatar shape="square" size={120} icon="user" src={paypal} /></Row>
+        </Col>
+      </Row>
+      <Row className={classes.btnContainer}>
+        <Button size='large' className={classes.btnStyle}>Proceed</Button>
+      </Row>
+      <Row className={classes.contactRow}>
+        <span className={classes.fontDecor}> Problem with payment? contact us on</span>
+        <span className={classes.linkDecor}> pay@traderlo.com</span>
+      </Row>
+    </>
+  );
+}
+
+export default ChooseMethod;
--- src/common/components/Payments/index.js
+++ src/common/components/Payments/index.js
@@ -1,44 +1,34 @@
-import React from 'react';
-import { Avatar, Divider, Row, Col, Button } from 'antd';
+import React, {useState} from 'react';
+import { Avatar, Divider, Row, Col, Button, Steps } from 'antd';
 
+//components
+import ChooseMethod from './Components/ChooseMethod';
 
-import CardItem from '../CardItem'
 //styles
 import classes from './styles.module.scss';
 
-//assets
-import bankCheck from 'assets/payments/bankCheck.png';
-import debitCards from 'assets/payments/debitCards.png';
-import paypal from 'assets/payments/paypal.png';
+
+
+const {Step} = Steps;
 
 
 const Payments = () => {
+  const [stepNo, setStepNo] = useState(0);
+
+  const onChange = current => {
+    setStepNo(current);
+  };
+
   return(
     <div className={classes.paymentWrap}>
     <span className={classes.header}>Just Few Clicks to finish you purchase! </span>
       <Divider />
-      <span className={classes.subHeader}>Select the Payment Method to make the payment</span>  
-        <Row className={classes.imageRow}>
-            <Col span={8}>
-                <Row><Avatar shape="square" size={120} icon="user" src={debitCards} /></Row>
-                <Row><span className={classes.fontDecor}> Credit / Debit Card</span></Row>
-            </Col>
-            <Col span={8}>
-                <Row><Avatar shape="square" size={120} icon="user" src={bankCheck} /></Row>
-                <Row><span className={classes.fontDecor}> Bank Account </span></Row>
-            </Col>
-            <Col span={8}>
-                <Row>{' '}</Row>
-                <Row><Avatar shape="square" size={120} icon="user" src={paypal} /></Row>
-            </Col>
-        </Row>
-        <Row className={classes.btnContainer}>
-        <Button size='large' className={classes.btnStyle}>Proceed</Button>
-        </Row>
-        <Row className={classes.contactRow}>
-          <span className={classes.fontDecor}> Problem with payment? contact us on</span>
-          <span className={classes.linkDecor}> pay@traderlo.com</span>
-        </Row>
+      <Steps current={stepNo} onChange={onChange} className={classes.stepper}>
+        <Step title="Select Method"/>
+        <Step title="Enter Details"/>
+        <Step title="Payment Done"/>
+      </Steps>
+      <ChooseMethod />
     </div>
   );
 }
