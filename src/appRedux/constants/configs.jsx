d484ad9ec82c89427a4441f67f5cfbcaaad1cf59
Angelica Capuz <angelica@stebr.com>
2020-03-08 21:50:37 +0800

    added content-type

--- src/appRedux/constants/configs.jsx
+++ src/appRedux/constants/configs.jsx
@@ -5,6 +5,7 @@ export const loginHeaders = {
   Authorization: 'Basic Y2xpZW50OnBhc3N3b3Jk' //TODO: to change
 };
 export const headers = {
+  'content-type': 'application/json',
   Accept: 'application/json',
   uid: '1',
   authorization: `Bearer ${window.localStorage.getItem('access_token')}`
