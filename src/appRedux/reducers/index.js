6d154b24cfc19fd9ba5f98af520fdebe01f459b1
Angelica Capuz <angelica@stebr.com>
2020-03-10 22:42:55 +0800

    [WIP] API for selling/domain

--- src/appRedux/reducers/index.js
+++ src/appRedux/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import * as home from './home';
+import * as sell from './sell';
 
 export default combineReducers({
-  ...home
+  ...home,
+  ...sell,
 });
--- src/appRedux/sagas/sell/domain.jsx
+++ src/appRedux/sagas/sell/domain.jsx
@@ -1,5 +1,8 @@
 import { all, call, fork, put, takeEvery } from "redux-saga/effects";
 
+//actions
+import { domain } from 'appRedux/actions/selling';
+
 //constants
 import { sellDomainTypes } from 'appRedux/constants/ActionTypes';
 import { base_url, headers } from 'appRedux/constants/configs';
@@ -8,9 +11,8 @@ import { base_url, headers } from 'appRedux/constants/configs';
 import { request, objToFormData } from 'common/utils/helpers';
 
 function* sellDomain({payload}) {
-  console.log('sell domain');
   try {
-    let resp = yield call(() => request.post(`${base_url}/selling/domain`,
+    const resp = yield call(() => request.post(`${base_url}/selling/domain`,
       {
         headers: {
           ...headers,
@@ -20,6 +22,9 @@ function* sellDomain({payload}) {
       }
     ));
     console.log('sellDomain resp: ', resp)
+    if(resp){
+      put(domain.sellDomainSuccess(resp));
+    }
   } catch (err) {
     console.log('err: ', err);
   }
