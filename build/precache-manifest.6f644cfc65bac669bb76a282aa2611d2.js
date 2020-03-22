e5fbbb7590fe86c406e14dc2722e3791e1a87ee4
vamsinadh <vb@stebrindia.com>
2020-03-22 10:19:59 +0530

    Pushing the build

---
+++ build/precache-manifest.6f644cfc65bac669bb76a282aa2611d2.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673873637aa8a5e328cf8499b338f296",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a4200b35ab5a5e70b9b",
+    "url": "./static/css/2.471598d2.chunk.css"
+  },
+  {
+    "revision": "f0e20fd582a229d1008a",
+    "url": "./static/css/main.cbf10f04.chunk.css"
+  },
+  {
+    "revision": "8a4200b35ab5a5e70b9b",
+    "url": "./static/js/2.5c404264.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "./static/js/2.5c404264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0e20fd582a229d1008a",
+    "url": "./static/js/main.2b116691.chunk.js"
+  },
+  {
+    "revision": "39ac97d225646a588f83",
+    "url": "./static/js/runtime-main.564c0cbb.js"
+  },
+  {
+    "revision": "a1bba5a42e5aa2505f51fb6b4f39f565",
+    "url": "./static/media/a1.a1bba5a4.png"
+  },
+  {
+    "revision": "486ec5025172f75a4edeeeae19e8703c",
+    "url": "./static/media/a2.486ec502.png"
+  },
+  {
+    "revision": "4a74ad7877f53e5fea556fad75612642",
+    "url": "./static/media/a3.4a74ad78.png"
+  },
+  {
+    "revision": "0ece2de996b7d382bcb386c594bad287",
+    "url": "./static/media/a4.0ece2de9.png"
+  },
+  {
+    "revision": "9f40037a0ba8888426ef9f9b00f6317d",
+    "url": "./static/media/a5.9f40037a.png"
+  },
+  {
+    "revision": "b4774f9ed458ad117c37018ed0b3e86c",
+    "url": "./static/media/buyerprotection.b4774f9e.png"
+  },
+  {
+    "revision": "fd89fa8ab9edd773fa69fb65bcaf1b35",
+    "url": "./static/media/debitCards.fd89fa8a.png"
+  },
+  {
+    "revision": "27ea356169156b427d8ea05044f834a1",
+    "url": "./static/media/editorchoice.27ea3561.png"
+  },
+  {
+    "revision": "e4db28e30184174613bbe3d06692d7ac",
+    "url": "./static/media/featured.e4db28e3.png"
+  },
+  {
+    "revision": "93f6abcf9b6f1985391f0e6235a7b986",
+    "url": "./static/media/graph.93f6abcf.png"
+  },
+  {
+    "revision": "0cfa477d85fd0d2ce9f1eb299cb99ea0",
+    "url": "./static/media/iconer.0cfa477d.jpg"
+  },
+  {
+    "revision": "9d7a52f73a2bbcb98b130b887cac7a50",
+    "url": "./static/media/logo.9d7a52f7.png"
+  },
+  {
+    "revision": "be97a4650d42aad97fe8e1d77a79d448",
+    "url": "./static/media/main.be97a465.jpg"
+  },
+  {
+    "revision": "bf0a1720992e1094bfe578824debc55f",
+    "url": "./static/media/paypal.bf0a1720.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "./static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "./static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "./static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "c8e19133bee22ccb4402d162395dc936",
+    "url": "./static/media/timeframe.c8e19133.png"
+  },
+  {
+    "revision": "e36b6b9d85131c1e0decebb000476892",
+    "url": "./static/media/trade.e36b6b9d.png"
+  },
+  {
+    "revision": "542368fbd529b0c5ef3fd979a8c1595b",
+    "url": "./static/media/websites.542368fb.png"
+  }
+]);
