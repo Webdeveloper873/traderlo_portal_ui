62da6667ec304190c9b68f5de5890733e548301e
Angelica Capuz <angelica@stebr.com>
2020-03-23 22:11:08 +0800

    Restructured user saga and reducer

--- src/appRedux/sagas/index.js
+++ src/appRedux/sagas/index.js
@@ -1,12 +1,13 @@
 import { all } from 'redux-saga/effects';
 
 // NOTE: import all sagas
-import { subscribeNews, blogs, user } from './home/index';
+import { subscribeNews, blogs } from './home/index';
 import { domain } from './sell';
 import { buyingDomain } from './buying';
 import { bidDomain } from './bidding';
 import payment from './payment';
 import {watchlist} from './userSidebar'
+import { user } from './user';
 
 export default function* rootSaga() {
   yield all([
