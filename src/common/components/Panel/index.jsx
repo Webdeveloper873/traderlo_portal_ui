9aa6dfff8c23631541c26e0cee0a3eb918037a1b
Angelica Capuz <angelica@stebr.com>
2020-03-28 21:37:33 +0800

    Added Password and Security page

--- src/common/components/Panel/index.jsx
+++ src/common/components/Panel/index.jsx
@@ -4,12 +4,12 @@ import { Row, Col } from 'antd';
 //styles
 import classes from './styles.module.scss';
 
-const Panel = ({ header, children }) => {
+const Panel = ({ header, className, children }) => {
   return(
-    <>
+    <div className={className}>
       {header ? <Row className={classes.panelHeader}>{header}</Row> : null}
       {children ? <Row className={classes.panelBody}>{children}</Row> : null}
-    </>
+    </div>
   );
 };
 
--- src/components/UserSecurity/components/PassAndSecurity/index.jsx
+++ src/components/UserSecurity/components/PassAndSecurity/index.jsx
@@ -1,13 +1,44 @@
 import React from 'react';
+import { Row, Col, Input, Typography, Switch } from 'antd';
 
 //components
 import Panel from 'common/components/Panel';
 
+//styles
+import classes from './styles.module.scss';
+
+//constants
+import { responsiveConf } from 'common/constants';
+
+const { threeCol } = responsiveConf;
+const { Title } = Typography;
+
 const PassAndSecurity = () => {
   return(
     <>
-      <Panel>
-        
+      <Panel header='Change Password'>
+        <Row gutter={24}>
+          <Col {...threeCol}>
+            <Input size='large' placeholder='Old Password' />
+          </Col>
+          <Col {...threeCol}>
+            <Input size='large' placeholder='New Password' />
+          </Col>
+          <Col {...threeCol}>
+            <Input size='large' placeholder='Re-Type Password' />
+          </Col>
+        </Row>
+      </Panel>
+      <Panel header='Two-factor authentication' className={classes.topMargin}>
+        <p className={classes.text}>Please download an authentication app such as 2FA Authenticator to your smartphone. Once activated, the app will generate a code that will be needed to unlock your account.</p>
+        <Col span={22}>
+          <p className={classes.text}>
+            While optional, using 2FA Authentication can increase the security of your account. Learn how to set up 2FA Authenticator or install 2FA Authenticator.
+          </p>
+        </Col>
+        <Col span={2}>
+          <Switch defaultChecked />
+        </Col>
       </Panel>
     </>
   );
