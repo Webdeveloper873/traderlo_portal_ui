26256de7bd00ead40d6b11748a53ce09482f744c
RC <ralph@stebr.co>
2020-04-12 14:54:22 +0800

    update

--- src/components/OAuth2RedirectHandler/index.jsx
+++ src/components/OAuth2RedirectHandler/index.jsx
@@ -7,6 +7,11 @@ import { useDispatch } from 'react-redux';
 import { user } from 'appRedux/actions/user';
 
 
+//constants
+import { routes } from 'common/constants';
+
+
+
 const OAuth2RedirectHandler  = () => {
     const dispatch = useDispatch();
     const getUrlParameter =(name) => {
@@ -17,23 +22,21 @@ const OAuth2RedirectHandler  = () => {
         return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
     };
 
-
-
         const token = getUrlParameter('token');
         const error = getUrlParameter('error');
 
         if(token) {
             //localStorage.setItem('access_token', token);
-            console.log(token,'to user action')
+
             dispatch(user.successViaGoogleLogin(token));
             return <Redirect to={{
-                pathname: "/user/dashboard",
+                pathname: routes.DASHBOARD_PAGE,
                 //state: { from: this.props.location }
             }}/>;
         } else {
             dispatch(user.failedGoogleLogin());
             return <Redirect to={{
-                pathname: "/", // <- redirect to homepage
+                pathname: routes.HOME_PAGE, // <- redirect to homepage
                 // state: {
                 //     from: this.props.location,
                 //     error: error
