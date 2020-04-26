81c9e948c0c5291b1ff800ecad894b3433a0da95
Angelica Capuz <angelica@stebr.com>
2020-04-26 21:10:48 +0800

    Fix Trad-240: Changed text when no cards/accounts

--- src/common/components/Payments/index.js
+++ src/common/components/Payments/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, {useState, useEffect} from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Divider } from 'antd';
 
 
@@ -31,15 +31,15 @@ const PageRouter = ({ selectedOpt, onClickOpt, stepNo, nextStep, isAddOnly}) => 
 
 
 const Payments = ({isAddOnly}) => {
-  console.log(isAddOnly,'isAddOnly');
   const [stepNo, setStepNo] = useState(0);
   const [selectedOpt, setSelectedOpt] = useState();
+  const savedBanks = useSelector(({ user }) => user.savedBanks);
+  const savedCards = useSelector(({ user }) => user.savedCards);
   const dispatch = useDispatch();
 
   useEffect(()=>{
     setStepNo(0);
     return () => {
-
       setStepNo(0);
       dispatch(user.getSavedBanks());
       dispatch(user.getSavedCard());
@@ -58,7 +58,13 @@ const Payments = ({isAddOnly}) => {
 
   return(
     <div className={classes.paymentWrap}>
-    <span className={classes.header}>Just Few Clicks to finish you purchase! </span>
+      <span className={classes.header}>
+        {
+          savedCards.length === 0 && savedBanks.length === 0 ?
+          'Hmm, It seems payment information is missing.' :
+          'Just Few Clicks to finish you purchase!'
+        }
+      </span>
       <Divider />
       <PageRouter
         stepNo={stepNo}
